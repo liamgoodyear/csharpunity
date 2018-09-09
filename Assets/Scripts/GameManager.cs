12866d6d6ef831ccdb3bb689653ec526b458d23c
liamgoodyear <liamdgoodyear@gmail.com>
2018-09-09 20:33:46 +0100

    game manager and kill

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    menu,
+    inGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour {
+
+    public GameState currentGameState = GameState.menu;
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        // StartGame();
+        currentGameState = GameState.menu;
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("s"))
+        {
+            StartGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        SetGameState(GameState.inGame);
+    }
+
+    public void GameOver()
+    {
+        SetGameState(GameState.gameOver);
+    }
+
+    public void BackToMenu()
+    {
+        SetGameState(GameState.menu);
+    }
+
+    void SetGameState(GameState newGameState)
+    {
+        if(newGameState == GameState.menu)
+        {
+
+        }
+        else if(newGameState == GameState.inGame)
+        {
+
+        }
+        else if(newGameState == GameState.gameOver)
+        {
+
+        }
+
+        currentGameState = newGameState;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float jumpForce = 25f;
+    public float runningSpeed = 1.5f;
+    private Rigidbody2D rb2d;
+    public Animator animator;
+
+    private void Awake()
+    {
+        animator.SetBool("isAlive", true);
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (GameManager.instance.currentGameState == GameState.inGame)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Jump();
+            }
+            animator.SetBool("isGrounded", IsGrounded());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (GameManager.instance.currentGameState == GameState.inGame)
+        {
+            if (rb2d.velocity.x < runningSpeed)
+            {
+                rb2d.velocity = new Vector2(runningSpeed, rb2d.velocity.y);
+            }
+        }
+    }
+
+    void Jump()
+    {
+        if (IsGrounded())
+        {
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    public LayerMask groundLayer;
+
+    bool IsGrounded()
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, groundLayer.value))
+        {
+            return true;
+        }
+        else
+        {          
+            return false;
+        }
+    }
+}
